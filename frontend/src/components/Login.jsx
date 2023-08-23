f144d1c83ad30c03819315807a9eca83ffee2b46
Shashikant Yadav <103162888+shashi310@users.noreply.github.com>
2023-08-23 23:34:01 +0530

    day-2 code added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Heading, Input, Button } from '@chakra-ui/react';
+import axios from 'axios';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8080/user/login', formData);
+      const token = response.data.token;
+
+      // Store the token in local storage
+      localStorage.setItem('token', token);
+
+      console.log('Login successful!', token);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  return (
+    <Box maxW="md" mx="auto" p={4}>
+      <Heading as="h2" mb={4}>
+        Login
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <Box mb={4}>
+          <label>Email:</label>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <label>Password:</label>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Box>
+        <Button type="submit" colorScheme="blue">
+          Log In
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default Login;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Heading, Input, Button } from '@chakra-ui/react';
+import axios from 'axios';
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Signup
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8080/user/register', formData);
+      console.log('Signup successful!', response.data);
+    } catch (error) {
+      console.error('Signup failed:', error);
+    }
+  };
+
+  return (
+    <Box maxW="md" mx="auto" p={4}>
+      <Heading as="h2" mb={4}>
+        Signup
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <Box mb={4}>
+          <label>Name:</label>
+          <Input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <label>Email:</label>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <label>Password:</label>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Box>
+        <Button type="submit" colorScheme="blue">
+          Sign Up
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default Signup;
