c025e78f0e850b0602c4c6851c62bb0bb9e18558
Vishal Giri <vishalgiri197@gmail.com>
2023-08-25 17:32:52 +0530

    NO_CHANGE_JUST_FOR_PULL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MainRoute from './routes/MainRoute';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div >
+      <Homepage/>
      <MainRoute />
     </div>
   );
